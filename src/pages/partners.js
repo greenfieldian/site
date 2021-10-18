ff54f63e4c0b99b92605f83c553f2e058e5e0cd2
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-10-18 12:56:28 -0500

    swiperjs

--- src/pages/partners.js
+++ src/pages/partners.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import Layout from '../components/layout'
 import Hero from '../components/hero'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import SystemsImg from "../assets/systems-photo.jpeg"
 import AWSLogo from "../assets/aws-logo.png"
@@ -12,6 +13,7 @@ import TableauLogo from "../assets/tableau-logo.png"
 import SalesforceLogo from "../assets/salesforce.png"
 
 import * as partnerStyles from "../styles/partners.module.css"
+import 'swiper/css'
 
 export default function Partners() {
     return (
@@ -52,6 +54,51 @@ export default function Partners() {
                                     </div>
                                     <div className={partnerStyles.core_partners_item}>
                                         <img src={SalesforceLogo}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={partnerStyles.additional_partner_container}>
+                        <div className="container">
+                            <div className="row">
+                                <header>
+                                    <h2 className={"section-header" + " " + partnerStyles.subsection_header}>But wait, there's more</h2>
+                                </header>
+                                <div className={partnerStyles.additional_main_container}>
+                                    <h3>We work with nearly every major CRM, analytics, development and digital experience partner.</h3>
+                                    <div className="">
+                                        <Swiper
+                                        spaceBetween={30}
+                                        slidesPerView={2}
+                                        onSlideChange={() => console.log('slide change')}
+                                        onSwiper={(swiper) => console.log(swiper)}
+                                        >
+                                            <SwiperSlide>
+                                                <div className={partnerStyles.additional_text_wrapper}>
+                                                    <p className={partnerStyles.item_title}>Google Cloud</p>
+                                                    <p className={partnerStyles.item_description}>Content management and multilevel marketing automation</p>
+                                                </div>
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <div className={partnerStyles.additional_text_wrapper}>
+                                                    <p className={partnerStyles.item_title}>Adobe</p>
+                                                    <p className={partnerStyles.item_description}>Content management and multilevel marketing automation</p>
+                                                </div>
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <div className={partnerStyles.additional_text_wrapper}>
+                                                    <p className={partnerStyles.item_title}>Wordpress</p>
+                                                    <p className={partnerStyles.item_description}>Content management and multilevel marketing automation</p>
+                                                </div>
+                                            </SwiperSlide>
+                                            <SwiperSlide>
+                                                <div className={partnerStyles.additional_text_wrapper}>
+                                                    <p className={partnerStyles.item_title}>Contentful</p>
+                                                    <p className={partnerStyles.item_description}>Content management and multilevel marketing automation</p>
+                                                </div>
+                                            </SwiperSlide>
+                                        </Swiper>
                                     </div>
                                 </div>
                             </div>
