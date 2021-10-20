4f4d9dc9c5235fa8660b4ee72b3cfe58102dbe3b
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-10-20 15:29:47 -0500

    commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Footer from './footer'
 import Header from './header'
 
+import "@fontsource/noto-sans"
+
 import './layout.css'
 
 export default function Layout({children}) {
